5dc212f2cd350e415e25e02cc19e4537d0a2cf5b
abhipandSogeti <abhishek.pandit@sogeti.com>
2025-04-04 11:54:57 +0200

    adds example c variations for page router, app router & DynamicIO

---
+++ 15-standard/next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8080",
+        pathname: "/images/**",
+      },
+    ],
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+};
+
+export default nextConfig;
